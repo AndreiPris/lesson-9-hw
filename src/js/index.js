ec836a05ebee890895a0a0dd21e1b4ad1ea8615b
AndreiPris <andreiprisacari2003@gmail.com>
2019-03-03 22:17:43 +0200

    Add Homework

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+var age = prompt('Write your age');
+if (age >= 25){
+    alert('I am over 25 years old');
+}
+else (age <= 25)[
+    alert('I am under 25')
+]
+    
+
+   
---
+++ src/js/index1.js
@@ -0,0 +1,3 @@
+var age = prompt('Write your age');
+name = (age >= 25) ?  "over 25 years old" :  "under 25";
+console.log('I am', name);
